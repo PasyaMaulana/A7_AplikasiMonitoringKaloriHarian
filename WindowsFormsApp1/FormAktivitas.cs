1da40910c0cbae55b3b230a65ff400c1c4046624
PasyaMaulana <pasyamaulana17@gmail.com>
2026-05-13 09:24:11 +0700

    setup form aktivitas

--- WindowsFormsApp1/FormAktivitas.cs
+++ WindowsFormsApp1/FormAktivitas.cs
@@ -12,9 +12,33 @@ namespace WindowsFormsApp1
 {
     public partial class FormAktivitas : Form
     {
+        private string connectionString =
+             "Server=PASYA\\PASYA;Database=AMKH;Integrated Security=True;";
+
+        private BindingSource bindingSource = new BindingSource();
+        private int selectedId = -1;
+
         public FormAktivitas()
         {
             InitializeComponent();
         }
-    }
+
+        private void FormAktivitas_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'aMKHDataSet.vw_AktivitasAktif' table. You can move, or remove it, as needed.
+            this.vw_AktivitasAktifTableAdapter.Fill(this.aMKHDataSet.vw_AktivitasAktif);
+            dgvAktivitas.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvAktivitas.MultiSelect = false;
+            dgvAktivitas.ReadOnly = true;
+            dgvAktivitas.AllowUserToAddRows = false;
+            dgvAktivitas.DataSource = bindingSource;
+
+
+            btnUpdate.Enabled = false;
+            btnHapus.Enabled = false;
+
+            MuatData();
+            TampilkanInfoAktivitas();
+
+        }
 }
