b22306c415e8dc6a202f1e25fc960653ebca860f
PasyaMaulana <pasyamaulana17@gmail.com>
2026-05-13 09:18:19 +0700

    form utama (selesai)

--- WindowsFormsApp1/FormUtama.cs
+++ WindowsFormsApp1/FormUtama.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApp1
+namespace AMKH_TESTING
 {
     public partial class FormUtama : Form
     {
