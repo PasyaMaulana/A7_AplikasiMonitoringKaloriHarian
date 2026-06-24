fdcb07bd40b281f63d6ea0d52f83f757de53fbbe
PasyaMaulana <pasyamaulana17@gmail.com>
2026-06-25 03:22:01 +0700

    menambahkan class ListKonsumsi

---
+++ WindowsFormsApp1/ListKonsumsi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AMKH
+{
+    public class ListKonsumsi
+    {
+        public string NamaItem { get; set; }
+        public decimal Kalori { get; set; }
+        public string Tipe { get; set; }
+        public DateTime TanggalKonsumsi { get; set; }
+        public decimal TargetKalori { get; set; }
+    }
+}
